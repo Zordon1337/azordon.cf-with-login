42ca3331a7123efd52c0ded207f3efb3b2804768
Zordon <65111609+Zordon1337@users.noreply.github.com>
2022-06-13 23:05:45 +0200

    q

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link href='https://fonts.googleapis.com/css?family=Alegreya Sans SC' rel='stylesheet'>
+<link rel="stylesheet" href="https://web.azordon.cf/css/main.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script>
+function cb(response) {
+document.getElementById('visits').innerText = response.value;
+}
+</script>
+<script async src="https://api.countapi.xyz/hit/azordon.cf/visits?callback=cb"></script>
+<style>
+</style>
+</head>
+<title>Zordon Official Site</title>
+</head>
+</html>
+<div class="topnav">
+    <a class="active" href="https://azordon.cf/index.html">Home</a>
+    <a href="https://web.azordon.cf/news.html">News</a>
+    <a href="https://web.azordon.cf/contact.html">Contact</a>
+    <a href="https://web.azordon.cf/about.html">About</a>
+    <a href="https://web.azordon.cf/projects.html">My Github Repos</a>
+    <a href="http://forum.azordon.cf/">Forum</a>
+  </div>>
+
+<div class="welcome">
+
+<h1>Welcome on my site here you can</h1>
+<p>download my roms</p>
+<p>download apps</p>
+<p>be up to date with news</p>
+<p></p>
+
+<p>sorry for changing domain to subdomain because some dumb thing was redirecting my site to page that was saying "YoUr WeBsItE wAs SuSpEnDeD" but i my other subdomains works without any problems<p>
+<p></p>
+
+</div>
+<div class="views">
+<p>Page views: <span id="visits"></p>
+</div>
